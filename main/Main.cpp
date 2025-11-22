a985bca0a45bdefc9a69f54970c54403ca9e343b
Thanh Pham <phamthanhkevin07@gmail.com>
2025-11-22 13:02:21 -0600

    BMP + ADXL + LSM work

--- main/Main.cpp
+++ main/Main.cpp
@@ -29,8 +29,8 @@
 
 // Sensor SPI init
 #define SPI_SCLK_PIN 12
-#define SPI_MISO_PIN 13
-#define SPI_MOSI_PIN 11
+#define SPI_MISO_PIN 13 //SDO
+#define SPI_MOSI_PIN 11 //SDA
 #define SPI_MAX_TRSZ 4096
 
 // SD+LoRa SPI Init
@@ -67,7 +67,7 @@ typedef struct{
     float bno_ori_w, bno_ori_x, bno_ori_y, bno_ori_z;
     float lsm_temp, adxl_temp, bno_temp;
     float bmp_temp, bmp_press, bmp_alt;
-}OutputData_t;
+}OutputData_t; //eventually to use to save to sd card and lora
 
 // typedef struct{
 //     float bmp_temp;
@@ -76,15 +76,15 @@ typedef struct{
 // } OutputData_t;
 
 typedef struct{
-    // OutputData_t *pOutputData;
-    // sensors_event_t *pEventADXL;
+    OutputData_t *pOutputData;
+    sensors_event_t *pEventADXL;
     sensors_event_t *pEventLSM_accel;
     sensors_event_t *pEventLSM_gyro;
     sensors_event_t *pEventLSM_temp;
 }TaskParams_t;
 
-// OutputData_t    gOutputData; /// global Output object for sensors data
-// sensors_event_t gEventADXL; /// global output data for ADXL375
+OutputData_t    gOutputData; /// global Output object for sensors data
+sensors_event_t gEventADXL; /// global output data for ADXL375
 sensors_event_t gEventLSM_accel; /// global output data for LSM acceleration
 sensors_event_t gEventLSM_gyro; /// global output data for LSM gyro
 sensors_event_t gEventLSM_temp; /// global output data for LSM temperature
@@ -93,8 +93,8 @@ uint32_t upTime; /// The current time of task is running in the beginning
 SemaphoreHandle_t gSpiMutex; /// SPI bus mutex to manage protocol traffic
 
 //Chip Object Instantiation
-// Adafruit_BMP5xx    BMP;
-// Adafruit_ADXL375   ADXL(ADXL375_CS, &SPI);
+Adafruit_BMP5xx    BMP;
+Adafruit_ADXL375   ADXL(ADXL375_CS, &SPI);
 Adafruit_LSM6DSO32 LSM;
 // Adafruit_BNO055 BNO(55, BNO055_ADDRESS_A, &Wire);
 // Adafruit_GPS GPS(&Wire);
@@ -102,8 +102,8 @@ Adafruit_LSM6DSO32 LSM;
 
 void init_spi() {
     SPI.begin(SPI_SCLK_PIN, SPI_MISO_PIN, SPI_MOSI_PIN, -1);
-    // BMP.begin(BMP581_CS, &SPI);
-    // ADXL.begin();
+    BMP.begin(BMP581_CS, &SPI);
+    ADXL.begin();
     LSM.begin_SPI(LSM6DSO32_CS, &SPI);
 }
 
@@ -140,8 +140,8 @@ extern "C" void app_main()
     gSpiMutex = xSemaphoreCreateMutex();
 
     TaskParams_t *pParams     = (TaskParams_t*)malloc(sizeof(TaskParams_t));
-    // pParams->pOutputData      = &gOutputData;
-    // pParams->pEventADXL       = &gEventADXL;
+    pParams->pOutputData      = &gOutputData;
+    pParams->pEventADXL       = &gEventADXL;
     pParams->pEventLSM_accel  = &gEventLSM_accel;
     pParams->pEventLSM_gyro   = &gEventLSM_gyro;
     pParams->pEventLSM_temp   = &gEventLSM_temp;
@@ -169,89 +169,89 @@ extern "C" void app_main()
 
 void Core0_task(void *pvParameter) {
     TaskParams_t    *pTask           = (TaskParams_t*)pvParameter;
-    // OutputData_t    *pOutputData     = pTask->pOutputData;
-    // sensors_event_t *pEventADXL      = pTask->pEventADXL;
+    OutputData_t    *pOutputData     = pTask->pOutputData;
+    sensors_event_t *pEventADXL      = pTask->pEventADXL;
     sensors_event_t *pEventLSM_accel = pTask->pEventLSM_accel;
     sensors_event_t *pEventLSM_gyro  = pTask->pEventLSM_gyro;
     sensors_event_t *pEventLSM_temp  = pTask->pEventLSM_temp;
 
     while(1)
     {
-      // /// BMP functions
-      // if (xSemaphoreTake(gSpiMutex, pdMS_TO_TICKS(10)) == pdTRUE) {
-      //     upTime = xTaskGetTickCount();
-      //     bool bmp_ok = BMP.performReading();
-      //     xSemaphoreGive(gSpiMutex);
+      /// BMP functions
+      if (xSemaphoreTake(gSpiMutex, pdMS_TO_TICKS(10)) == pdTRUE) {
+          upTime = xTaskGetTickCount();
+          bool bmp_ok = BMP.performReading();
+          xSemaphoreGive(gSpiMutex);
 
-      //     if (bmp_ok) {
-      //         pOutputData->bmp_temp  = BMP.temperature;
-      //         pOutputData->bmp_press = BMP.pressure;
-      //         pOutputData->bmp_alt   = BMP.readAltitude(1013.25f);
+          if (bmp_ok) {
+              pOutputData->bmp_temp  = BMP.temperature;
+              pOutputData->bmp_press = BMP.pressure;
+              pOutputData->bmp_alt   = BMP.readAltitude(1013.25f);
 
-      //     #ifdef DEBUG
-      //         printf("BMP Up Time: %lu [ms]\n", (unsigned long)upTime);
-      //         printf("bmp_temp: %f\n", pOutputData->bmp_temp);
-      //         printf("bmp_press: %f\n", pOutputData->bmp_press);
-      //         printf("bmp_alt: %f\n\n", pOutputData->bmp_alt);
-      //     #endif
-      //     }
-      // } else {
-      //     #ifdef DEBUG
-      //         printf("No token BMP!!!");
-      //     #endif
-      // }
+          #ifdef DEBUG
+              printf("BMP Up Time: %lu [ms]\n", (unsigned long)upTime);
+              printf("bmp_temp: %f\n", pOutputData->bmp_temp);
+              printf("bmp_press: %f\n", pOutputData->bmp_press);
+              printf("bmp_alt: %f\n\n", pOutputData->bmp_alt);
+          #endif
+          }
+      } else {
+          #ifdef DEBUG
+              printf("No token BMP!!!");
+          #endif
+      }
           
-        
+    vTaskDelay(pdMS_TO_TICKS(30));
 
-      // /// ADXL function
-      // if (xSemaphoreTake(gSpiMutex, pdMS_TO_TICKS(10)) == pdTRUE) {
-      //   upTime = xTaskGetTickCount();
-      //   bool adxl_ok = ADXL.getEvent(pEventADXL);
-      //   xSemaphoreGive(gSpiMutex);
+      /// ADXL function
+      if (xSemaphoreTake(gSpiMutex, pdMS_TO_TICKS(10)) == pdTRUE) {
+        upTime = xTaskGetTickCount();
+        bool adxl_ok = ADXL.getEvent(pEventADXL);
+        xSemaphoreGive(gSpiMutex);
 
-      //   if (adxl_ok) {
-      //     #ifdef DEBUG
-      //             printf("ADXL Up Time: %lu [ms]\n", (unsigned long)upTime);
-      //             printf("X: %f [m/s^2]\n", pEventADXL->acceleration.x);
-      //             printf("Y: %f [m/s^2]\n", pEventADXL->acceleration.y);
-      //             printf("Z: %f [m/s^2]\n\n", pEventADXL->acceleration.z);
-      //     #endif
-      //   }
-      // } else {
-      //     #ifdef DEBUG
-      //         printf("No token ADXL!!!");
-      //     #endif
-      // }
+        if (adxl_ok) {
+          #ifdef DEBUG
+                  printf("ADXL Up Time: %lu [ms]\n", (unsigned long)upTime);
+                  printf("X: %f [m/s^2]\n", pEventADXL->acceleration.x);
+                  printf("Y: %f [m/s^2]\n", pEventADXL->acceleration.y);
+                  printf("Z: %f [m/s^2]\n\n", pEventADXL->acceleration.z);
+          #endif
+        }
+      } else {
+          #ifdef DEBUG
+              printf("No token ADXL!!!");
+          #endif
+      }
 
+      vTaskDelay(pdMS_TO_TICKS(30));
 
+      // LSM function 
+      if (xSemaphoreTake(gSpiMutex, pdMS_TO_TICKS(10)) == pdTRUE) {
+        upTime = xTaskGetTickCount();
+        bool lsm_ok = LSM.getEvent(pEventLSM_accel, pEventLSM_gyro, pEventLSM_temp);
+        xSemaphoreGive(gSpiMutex);
 
-      // // LSM function 
-      // if (xSemaphoreTake(gSpiMutex, pdMS_TO_TICKS(10)) == pdTRUE) {
-      //   upTime = xTaskGetTickCount();
-      //   bool lsm_ok = LSM.getEvent(pEventLSM_accel, pEventLSM_gyro, pEventLSM_temp);
-      //   xSemaphoreGive(gSpiMutex);
+        if (lsm_ok) {
+          #ifdef DEBUG
+            printf("LSM Up Time: %lu [ms]\n", (unsigned long)upTime);
+            printf("X Acceleration: %f [m/s^2]\n", pEventLSM_accel->acceleration.x);
+            printf("Y Acceleration: %f [m/s^2]\n", pEventLSM_accel->acceleration.y);
+            printf("Z Acceleration: %f [m/s^2]\n", pEventLSM_accel->acceleration.z);
 
-      //   if (lsm_ok) {
-      //     #ifdef DEBUG
-      //       printf("LSM Up Time: %lu [ms]\n", (unsigned long)upTime);
-      //       printf("X Acceleration: %f [m/s^2]\n", pEventLSM_accel->acceleration.x);
-      //       printf("Y Acceleration: %f [m/s^2]\n", pEventLSM_accel->acceleration.y);
-      //       printf("Z Acceleration: %f [m/s^2]\n", pEventLSM_accel->acceleration.z);
+            printf("X Gyro: %f\n", pEventLSM_gyro->gyro.x);
+            printf("Y Gyro: %f\n", pEventLSM_gyro->gyro.y);
+            printf("Z Gyro: %f\n", pEventLSM_gyro->gyro.z);
 
-      //       printf("X Gyro: %f\n", pEventLSM_gyro->gyro.x);
-      //       printf("Y Gyro: %f\n", pEventLSM_gyro->gyro.y);
-      //       printf("Z Gyro: %f\n", pEventLSM_gyro->gyro.z);
+            printf("Temp: %f\n\n", pEventLSM_temp->temperature);
+          #endif
+        }
+      } else {
+          #ifdef DEBUG
+              printf("No token LSM!!!");
+          #endif
+      }
 
-      //       printf("Temp: %f\n\n", pEventLSM_temp->temperature);
-      //     #endif
-      //   }
-      // } else {
-      //     #ifdef DEBUG
-      //         printf("No token LSM!!!");
-      //     #endif
-      // }
-
-
+      vTaskDelay(pdMS_TO_TICKS(30));
 
 
 
