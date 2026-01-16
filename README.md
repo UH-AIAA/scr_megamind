612d7e4b8fc9d34b7723d1aa9429808b2f63416e
Thanh Pham <phamthanhkevin07@gmail.com>
2026-01-15 19:40:50 -0600

    Update config & Start indenpendent test structure

--- README.md
+++ README.md
@@ -12,3 +12,22 @@
   -  ReadMe for your code
   -  Error handling. Print out to serial monitor sensor/state error
   -  Automate if necessary
+<br><br><br>
+# Setting up test dependencies
+
+## Overview
+  - Due to highly complexity of the state machine for many safety reason and filtering to prevent invalid data from sensors, GTest will be used to ensure failure cases are caught in time and prevent effectively during flight operation.
+  - The folder **test_components** contains Google Test Framework version 1.17.0 on 04/30/2025
+## Setup Example
+  - Since we are using ESP-IDF framework, we can use the ESP-IDF terminal to run our necessary commands
+  - `cd` into the example folder
+  - `mkdir build` to create a build folder 
+  - `cd` into build folder and do `cmake ..`
+  - You should see something like : <br>
+      -- Looking for pthread_create in pthread - not found <br>
+      -- Found Threads: TRUE <br>
+      -- Configuring done (8.2s) <br>
+      -- Generating done (0.2s) <br>
+  - Do `cmake --build .` This will create an executable in **build/Debug/example_test.exe**
+  - Do `.\Debug\example_test.exe` to run test
+  - Enjoy the green (or red) lines from your test!
--- main/src/prototype_main.cpp
+++ main/src/prototype_main.cpp
@@ -18,9 +18,7 @@
 #include "Adafruit_GPS.h"
 #include "LoRa.h"
 #include "SD.h"
-
-/// Hardware Pins import
-#include "Pins.h"
+#include "prototype_main.h"
 
 /// Define frequency Lora + SD
 #define LORA_FREQ 915E6 /// 915MHz frequency for Lora
@@ -28,42 +26,6 @@
 
 /// Debug control 
 #define DEBUG
-
-/// @brief 
-/*      
-        1. Calibrated Data struct
-        2. SPI & I2C buses, sensors data and GPS fix validation
-        3. Flight State
-        4. Apogee
-*/
-typedef struct{
-    float bmp_temp, bmp_press, bmp_alt = 0;
-    float adxl_acc_x, adxl_acc_y, adxl_acc_z, adxl_temp = 0;
-    float lsm_acc_x, lsm_acc_y, lsm_acc_z,
-          lsm_gyro_x, lsm_gyro_y, lsm_gyro_z,
-          lsm_temp = 0;
-    float bno_quar_w, bno_quar_x, bno_quar_y, bno_quar_z,
-          bno_acc_x,  bno_acc_y,  bno_acc_z,
-          bno_gyro_x, bno_gyro_y, bno_gyro_z,
-          bno_mag_x,  bno_mag_y,  bno_mag_z,
-          bno_ori_x,  bno_ori_y,  bno_ori_z = 0;
-    uint8_t gps_sats = 0;
-    float gps_lat, gps_long, gps_alt = 0;
-    
-    bool spi1_ok = false;
-    bool spi2_ok = false;
-    bool i2c_ok = false;
-    bool bmp_ok = false;
-    bool adxl_ok = false;
-    bool lsm_ok = false;
-    bool bno_ok = false;
-    bool gpsFix_ok = false;
-    bool sd_ok = false;
-    bool lora_ok = false;
-
-    uint8_t flightState = 0;
-    float bmp_apogee_record = 0;
-} OutputData_t; 
 
 /// @brief Object for sensors data retrieving
 /* 
@@ -261,16 +223,6 @@ void init_I2C() {
     /// Initialize BNO
     BNO.begin();
 }
-
-void Core0_BMP_task(void *pvParameter);
-void Core0_ADXL_task(void *pvParameter);
-void Core0_LSM_task(void *pvParameter);
-void Core0_stateMachine(void *pvParameter);
-
-void Core1_BNO_task(void *pvParameter);
-void Core1_GPS_task(void *pvParameter);
-void Core1_SD_task(void *pvParameter);
-void Core1_Lora_task(void *pvParameter);
 
 extern "C" void app_main()
 {
@@ -702,7 +654,6 @@ void Core1_BNO_task(void *pvParameter) {
 
 // TODO: [NS] look at safety structures in this function
 void Core1_GPS_task(void *pvParameter) {
-    while (1) {
         uint8_t m_bytes_processed = 0;
         bool  m_got_fix     = false;
         uint32_t cycle_start = millis();
@@ -760,7 +711,6 @@ void Core1_GPS_task(void *pvParameter) {
             gOutputData.gpsFix_ok = false;
         }
         vTaskDelay(pdMS_TO_TICKS(500)); // Stable, Unoptimized
-    }
 }
 
 void Core1_SD_task(void *pvParameter) {
