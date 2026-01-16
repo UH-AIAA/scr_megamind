9a3e4bd0a53017dc22d9b1350744241a9e0c514f
Thanh Pham <phamthanhkevin07@gmail.com>
2026-01-15 19:38:30 -0600

    Prototyping independent test structure in mai & Update configs to 5.5.1

---
+++ example_testing/build/googletest_build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/phamt/Downloads/scr_megamind/test_components/googletest/googletest
+# Build directory: C:/Users/phamt/Downloads/scr_megamind/example_testing/build/googletest_build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
