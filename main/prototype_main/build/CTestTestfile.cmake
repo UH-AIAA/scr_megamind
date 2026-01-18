efbbb577930ed9d0f91287b8d90ed607b27b318e
Thanh Pham <phamthanhkevin07@gmail.com>
2026-01-17 21:44:13 -0600

    Implement OpenCpp Coverage + remove tracked build files

--- main/prototype_main/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/phamt/Downloads/scr_megamind/main/prototype_main
-# Build directory: C:/Users/phamt/Downloads/scr_megamind/main/prototype_main/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/phamt/Downloads/scr_megamind/main/prototype_main/build/prototypeMain_test[1]_include.cmake")
-subdirs("googletest_build")
